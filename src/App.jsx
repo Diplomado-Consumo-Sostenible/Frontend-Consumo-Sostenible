8e4973816200b7534ea16d602bf530724adf14d8
JOSSEyela <124942016+JOSSEyela@users.noreply.github.com>
2026-03-30 22:09:08 -0500

    Initial Commit- Creacion de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section className="hero">
+        <h1 Class="text-amber-400 text-5xl"> Consumo Sostenible </h1>
+      </section>
+        
+    </>
+  )
+}
+
+export default App
